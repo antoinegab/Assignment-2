4a5c0d2713ef0c8a93c4c9574b34f4bd56d9b02e
antoinegab <antoinegabriel@hotmail.com>
2015-10-22 17:08:30 +0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+require('connect.php');
+if(isset($_POST['Username']) && $_POST['Username'] !=null){
+	$Username= $mysqli->real_escape_string($_POST['Username']);
+} 
+else {
+	die("bede masesak ye");
+}
+if(isset($_POST['Loginname']) && $_POST['Loginname'] !=null){
+	$Loginname=$mysqli->real_escape_string($_POST['Loginname']);
+	
+} 
+else {
+	die("bede masesak ye");
+}
+if(isset($_POST['Email']) && $_POST['Email'] !=null){
+	$Email=$mysqli->real_escape_string($_POST['Email']);
+	
+} 
+else {
+	die("bede masesak ye");
+}
+if(isset($_POST['Guestbook']) && $_POST['Guestbook'] !=null){
+	$Guestbook=$mysqli->real_escape_string($_POST['Guestbook']);
+	
+} 
+else {
+	die("bede masesak ye");
+}
+
+
+
+$exist = $mysqli->prepare("SELECT * FROM User WHERE Email = ?");
+$exist->bind_param('s', $Email);
+$exist->execute();
+$exist->store_result();
+$num = $exist->num_rows;
+$exist->bind_result($UserID, $Username, $Loginname, $Email, $Guestbook);
+$exist->fetch();
+
+if($num == 0 )
+
+{
+
+
+$stmt =$mysqli->prepare("INSERT INTO User(Username, Loginname, Email, Guestbook) Values (?,?,?,?)");
+	$stmt->bind_param('ssss',$Username, $Loginname, $Email, $Guestbook);
+	$stmt->execute();	
+	$stmt->close();
+	$mysqli->close();
+
+die("Succesfully added");
+}else
+{
+die("User already exists: ".$Username.", Message: ".$Guestbook." :D");
+
+}
+
+
+
+$stmt->execute();
+$stmt->close();
+header("Location:index3.html");
+
+?>

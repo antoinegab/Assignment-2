15eabba4f290e34047cea42070b8b9c9dacd9692
antoinegab <antoinegabriel@hotmail.com>
2015-10-26 15:42:18 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 $host = "mysql10.000webhost.com";
 $db_name = "a4989387_ass1";
-$user = "a4989387_root";
-$password = "myspace123";
+$user = "******";
+$password = "******";
 $mysqli =new mysqli($host, $user, $password, $db_name);
 if(mysqli_connect_errno())
 {
